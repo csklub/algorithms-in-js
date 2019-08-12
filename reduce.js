d30a5e89fc69864f767e20ca9696a5a4ade9d87c
me-x-mi <ezrqnkemboi@gmail.com>
2019-08-13 00:19:13 +0300

    add example of reduce fn in js

---
+++ reduce.js
@@ -0,0 +1,48 @@
+/**
+ * This is an example of the use of .reduce() method in js
+ * We will take the add ages of all people that exist in community between 20-24.
+ * The people are grouped according to their ages
+ */
+
+const peopleInCommunity = [
+    {
+        age: 20,
+        noOfPeople: 2
+    },
+    {
+        age: 21,
+        noOfPeople: 4
+    },
+    {
+        age: 22,
+        noOfPeople: 12
+    },
+    {
+        age: 23,
+        noOfPeople: 1
+    },
+    {
+        age: 24,
+        noOfPeople: 0
+    }
+]
+/**
+ * Write reduce method
+ * The reducer function takes four arguments:
+ * - Accumulator (acc)
+ * - Current Value (cur)
+ * - Current Index (idx) - Optional
+ * - Source Array (src) - Optional
+ * - Initial value - Optional
+ *  Read more on: 
+ *  https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/Reduce
+ */
+const totalAgeValue = peopleInCommunity.reduce(
+    // Destructure age and noOfPeople from peopleInCommunity array
+    (accumulator, { age, noOfPeople }) =>
+        //  Multiply the age and noOfPeople and add to accumulator
+        accumulator + (age * noOfPeople)
+    , 0)
+
+// Log value of total age manipulations
+console.log('Average age: ', totalAgeValue)
---
+++ reduce.md
@@ -0,0 +1,4 @@
+## Reduce Examples 
+Show how `.reduce()` works in javascript. 
+
+[Reference Link](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/Reduce)
